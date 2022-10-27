6d393cdfe43961e8ac3b92bf8ee2d74a77caa5c3
T Vijay Kumar <yourstrulyvijay12@gmail.com>
2022-10-27 15:47:28 +0530

    Modifications 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
+  window.scrollTo(0, 0);
 }
 
 // Email Js
@@ -10,45 +10,52 @@ let fmessage = "";
 
 function enquiry() {
 
-    femail = document.getElementById('inputEmail4').value;
-    fname = document.getElementById('inputAddress').value;
-    fphone = document.getElementById('inputPassword4').value;
-    fmessage = document.getElementById('exampleFormControlTextarea1').value;
+  femail = document.getElementById('inputEmail4').value;
+  fname = document.getElementById('inputAddress').value;
+  fphone = document.getElementById('inputPassword4').value;
+  fmessage = document.getElementById('exampleFormControlTextarea1').value;
 
-    emailjs.send("service_5j95idg", "template_tdtcz3c", {
-        from_name: fname,
-        email: femail,
-        phone: fphone,
-        message: fmessage,
-    });
+  if (fname == "" || fphone == "") {
+    alert("Kindly fill the mandatory details! Name & Phone No. is must!");
+  }
+  else {
+
+    // emailjs.send("service_5j95idg", "template_tdtcz3c", {
+    //     from_name: fname,
+    //     email: femail,
+    //     phone: fphone,
+    //     message: fmessage,
+    // });
 
     document.getElementById('inputEmail4').value = "";
     document.getElementById('inputAddress').value = "";
     document.getElementById('inputPassword4').value = "";
     document.getElementById('exampleFormControlTextarea1').value = "";
-}
 
+    alert("Successfully submitted! We will contact you soon!")
+  }
+}
 document.getElementById("submit").onclick = enquiry;
 
 function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-    }
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
   }
+}
 
 
-  // When the user scrolls the page, execute myFunction
-window.onscroll = function() {scr()};
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () { scr() };
 
 // Get the navbar
 var navbar = document.getElementsByClassName("navbar")[0];
